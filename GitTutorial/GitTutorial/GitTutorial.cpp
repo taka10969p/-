58269421dadf360a87c0a3db90bd55e9b9b0acb4
Daaata <t107590035@ntut.org.tw>
2020-03-09 17:04:46 +0800

    練習 7_OK

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -1,14 +1,25 @@
 ﻿#include "stdafx.h"
 #include <string>
 #include <iostream>
+
 using namespace System;
-bool Guess(int number) {
+
+bool Guess(int number) 
+{
+	static int target = -1;
+	if (target == -1) 
+	{
+		Random r;
+		target = r.Next() % 100 + 1;
+	}
+	if (number == target)return true;
 	return false;
 }
-int main(array<System::String ^> ^args)
+int main(array<System::String^>^ args)
 {
 	int guess;
-	do {
+	do 
+	{
 		std::cin >> guess;
 	} while (!Guess(guess));
 	return 0;

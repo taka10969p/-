ae54f40a6356236b2b0682d95ed6e9f3ebf5a86a
taka10969p <ashin147258@gmail.com>
2020-03-09 17:20:33 +0800

    no message

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -3,12 +3,23 @@
 #include <iostream>
 using namespace System;
 bool Guess(int number) {
+	static int target = -1;
+	if (target == -1) {
+		Random r;
+		target = r.Next() % 100 + 1;
+	}
+	if (number == target) {
+		std::cout << "Correct !!";
+		target = -1;
+		return true;
+	}
+	else std::cout << "Wrong" << std::endl;
+	return false;
 }
 int main(array<System::String ^> ^args)
 {
 	int guess;
 	do {
-		std::cout << "Choose a number between 1 - 100 :";
 		std::cin >> guess;
 	} while (!Guess(guess));
 	return 0;
